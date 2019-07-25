ec9babe1a93524d15a7b05ecc6df37fe6c0199ca
Tanmoy Dhar <30317977+tdrana@users.noreply.github.com>
2019-07-25 12:49:21 +0600

    1st update

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 function performGetRequest1(){
     var resultElement = document.getElementById('getResult1');
     resultElement.innerHTML ='';
-    axios.get('http://jsonplaceholder.typicode.com/todos')
+    axios.get('https://jsonplaceholder.typicode.com/todos')
          .then(function(response){
              resultElement.innerHTML = generateSuccessHTMLOutput(response);
          })
